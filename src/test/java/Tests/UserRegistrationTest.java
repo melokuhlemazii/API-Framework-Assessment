4700496676f613981f070d8006b8e6aaaf7a3ff5
melokuhlemazii <melomazibuko8@gmail.com>
2026-03-30 13:39:39 +0200

    Added Database Connection

--- src/test/java/Tests/UserRegistrationTest.java
+++ src/test/java/Tests/UserRegistrationTest.java
@@ -1,17 +1,25 @@
 package Tests;
 
 import RequestBuilder.ApiRequestBuilder;
+import Utilities.DatabaseConnection;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class UserRegistrationTest {
 
+    @BeforeClass
+    public void setup() throws Exception {
+        DatabaseConnection.dbConnection();
+    }
+
     @Test
     public void adminLoginTest() {
 
-        ApiRequestBuilder.loginUserResponse("spare@admin.com", "@12345678")
+        ApiRequestBuilder.loginUserResponse(DatabaseConnection.getEmail, DatabaseConnection.getPassword)
                 .then()
                 .log().all()
                 .assertThat()
                 .statusCode(200);
+
     }
 }
--- src/test/java/Utilities/DatabaseConnection.java
+++ src/test/java/Utilities/DatabaseConnection.java
@@ -1,4 +1,31 @@
 package Utilities;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+
+    public static String getEmail;
+    public static String getPassword;
+
+    public static void dbConnection() throws SQLException {
+        String dbURL = "jdbc:mysql://102.222.124.22:3306/ndosian6b8b7_teaching";
+        String dbUsername = "ndosian6b8b7_teaching";
+        String dbPassword = "^{SF0a=#~[~p)@l1";
+
+        try (Connection connection = java.sql.DriverManager.getConnection(dbURL, dbUsername, dbPassword)) {
+            try (java.sql.Statement statement = connection.createStatement();
+                 java.sql.ResultSet resultSet = statement.executeQuery("SELECT * FROM loginUser WHERE id = 1")) {
+                while (resultSet.next()) {
+                    getEmail = resultSet.getString("email");
+                    getPassword = resultSet.getString("password");
+                    System.out.println("Email: " + getEmail + ", Password: " + getPassword);
+                }
+            } catch (SQLException e) {
+                System.out.println("Error executing query: " + e.getMessage());
+            }
+        } catch (SQLException e) {
+            System.out.println("Error connecting to database: " + e.getMessage());
+        }
+    }
 }
